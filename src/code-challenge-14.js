b4a151d1ea41d8fb6d96e565a774d7b4ab786e75
cmmerritt <christiane.merritt@gmail.com>
2021-05-15 23:11:47 -0700

    two tests pass

--- src/code-challenge-14.js
+++ src/code-challenge-14.js
@@ -1,4 +1,11 @@
+// ok I swear the challenges were different when I first started working on this a few days ago, because I had tests for different functions in a doc, so I had to spend a while figuring out what exactly is going on
+
 export function sortByChildren(arr) {
   return arr.sort((a, b) => 
     a.children.length - b.children.length);
 }
+
+export function replaceZeros(str) {
+  return str.replace(/0/g, 'zero');
+}
+
